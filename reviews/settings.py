c44ed514593cdb788a8670879cf65430383d8d88
go-max07 <johnmetry17@gmail.com>
2021-09-21 12:02:31 +0300

    Updated bootstrap to installed apps

--- reviews/settings.py
+++ reviews/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards.apps.AwardsConfig',
-    'project.apps.ProjectConfig',
+    'projects.apps.ProjectsConfig',
+    'bootstrap4',
+
+
 ]
 
 MIDDLEWARE = [
