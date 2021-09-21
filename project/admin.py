815b0d9f5463a28c26ec03819854e5681944ba16
go-max07 <johnmetry17@gmail.com>
2021-09-21 09:43:01 +0300

    Registered models to admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Technology, Project, Detail
+
+admin.site.register(Project)
+admin.site.register(Author)
+admin.site.register(Technology)
+admin.site.register(Detail)
